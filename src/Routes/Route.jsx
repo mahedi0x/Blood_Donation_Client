4862df159c5195c552e3f5fe56819f0cf94b77bf
Mahedi0x <mahedih.cse@gmail.com>
2025-12-18 23:17:00 +0600

    modify my profile page

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,43 +1,95 @@
 import { createBrowserRouter } from "react-router";
-import RootLayout from "../Layouts/RootLayout";
 
 import Home from "../Pages/Home/Home";
 import AuthLayout from "../Layouts/AuthLayout";
-import About from "../Components/About";
 import Login from "../Pages/Auth/Login";
 import Register from "../Pages/Auth/Register";
+import DashboardLayout from "../Layouts/DashboardLayout";
+import ProfilePage from "../Pages/Dashboard/ProfilePage";
+import MyDonationRequests from "../Pages/Dashboard/MyDonationRequests";
+import CreateDonationRequest from "../Pages/Dashboard/CreateDonationRequest";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+import ManageDonationRequest from "../Pages/Dashboard/ManageDonationRequest";
+import DonationRequestDetails from "../Pages/Dashboard/DonationRequestDetails";
+import About from "../Components/About";
+import DonorSearch from "../Pages/DonorSearch";
+import DonationRequests from "../Pages/DonationRequests";
+import PrivateRoute from "../Context/PrivateRoute";
+import RootLayout from "../Layouts/RootLayout";
+
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      Component: RootLayout,
-      hydrateFallbackElement: <div>Loading...</div>,
-      children: [
-        {
-          index: true,
-          Component: Home,
-        },
-        {
-          path: "about",
-          Component: About,
-        },
-      ],
-    },
-    {
-        path: "/",
-        Component: AuthLayout,
-    
-        children: [
-          {
-            path: "login",
-            Component: Login,
-          },
-          {
-            path: "register",
-            Component: Register,
-            loader: () => fetch("./districts.json"),
-          },
-        ],
+  {
+    path: "/",
+    Component: RootLayout,
+    hydrateFallbackElement: <div>Loading...</div>,
+    children: [
+      {
+        index: true,
+        Component: Home,
       },
-   
-  ]);
-  
+      {
+        path: "about",
+        Component: About,
+      },
+      {
+        path: "search",
+        Component: DonorSearch,
+        loader: () => fetch("/districts.json"),
+      },
+      {
+        path: "donation-requests",
+        Component: DonationRequests,
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+
+    children: [
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "register",
+        Component: Register,
+        loader: () => fetch("./districts.json"),
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    Component: DashboardLayout,
+    hydrateFallbackElement: <div>Loading...</div>,
+    children: [
+      { index: true, Component: Dashboard },
+      {
+        path: "my-profile",
+        Component: ProfilePage,
+      },
+      {
+        path: "my-donation-requests",
+        Component: MyDonationRequests,
+      },
+      {
+        path: "create-donation-request",
+        Component: CreateDonationRequest,
+        loader: () => fetch("/districts.json"),
+      },
+      {
+        path: "manage-donation-request/:id",
+        Component: ManageDonationRequest,
+      },
+      {
+        path: "donation-request-details/:id",
+        element: (
+          <PrivateRoute>
+            <DonationRequestDetails></DonationRequestDetails>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+
+]);
