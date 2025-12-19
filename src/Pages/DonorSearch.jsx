3fdadc8998933779d106732fd358287d472c492b
Mahedi0x <mahedih.cse@gmail.com>
2025-12-19 10:38:05 +0600

    create donation search page

---
+++ src/Pages/DonorSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DonorSearch = () => {
+    return (
+        <div>
+            <h2>Donation search page..</h2>
+        </div>
+    );
+};
+
+export default DonorSearch;
